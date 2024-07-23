9b9a751aa256786b08dad8af7a8da2355fed255f
abitha0020 <132005925+abitha0020@users.noreply.github.com>
2024-07-23 16:38:44 +0530

    Update index.tsx

--- shoppingCart/src/Routes/index.tsx
+++ shoppingCart/src/Routes/index.tsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router";
 import Home from "@/pages/home";
+import CartPage from "@/pages/cartPage";
 
 export default function Navigator(){
     return(
         <>
             <Routes>
                 <Route path={"/"} element={<Home />}></Route>
+                <Route path={"/cart"} element={<CartPage/>}></Route>
             </Routes>
         </>
     )
