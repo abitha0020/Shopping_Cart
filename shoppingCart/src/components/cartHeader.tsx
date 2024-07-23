6a6d796eb30e7bbb9c9f39dd62d0e9531cfedfb0
abitha0020 <132005925+abitha0020@users.noreply.github.com>
2024-07-23 21:12:46 +0530

    Create cartHeader.tsx

---
+++ shoppingCart/src/components/cartHeader.tsx
@@ -0,0 +1,22 @@
+import { Button } from "./ui/button";
+import { Card,
+    CardFooter
+ } from "./ui/card";
+type cartHeaderProps ={
+    subtotal: number,
+}
+
+export default function CartHeader({subtotal} : cartHeaderProps) {
+    return(
+        <div className="w-2/5">
+            <Card className="m-20 px-20">
+                <h2 className="text-center pt-10 text-2xl font-medium">Your Cart</h2>
+                <h3 className="text-lg font-base text-center pt-5">Subtotal  ${subtotal}</h3>
+                <CardFooter className="flex flex-col px-20 gap-2 mt-5">
+                    <Button variant={"destructive"}>Proceed to buy</Button>
+                    <Button> Clear Cart</Button>
+                </CardFooter>
+            </Card>
+        </div>
+    ); 
+}
