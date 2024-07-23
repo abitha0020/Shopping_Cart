ecf9b74b006e82a8aa6f56713dfdc9cc92179a7a
abitha0020 <132005925+abitha0020@users.noreply.github.com>
2024-07-23 16:38:39 +0530

    Create cartPage.tsx

---
+++ shoppingCart/src/pages/cartPage.tsx
@@ -0,0 +1,11 @@
+import CartCard from "@/components/cartcard";
+import Navbar from "@/components/navbar";
+export default function CartPage() {
+    return (
+        <div>
+            <Navbar/>
+            <CartCard/>
+            
+        </div>
+    );
+}
