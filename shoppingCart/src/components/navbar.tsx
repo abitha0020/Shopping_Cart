56bfcd545afbe58f181f85a054c15d94c644e509
abitha0020 <132005925+abitha0020@users.noreply.github.com>
2024-07-23 12:59:04 +0530

    Create navbar.tsx

---
+++ shoppingCart/src/components/navbar.tsx
@@ -0,0 +1,18 @@
+function Navbar(){
+    return(
+        <nav>
+            <div className="px-20 mx-auto flex justify-between h-[100px] bg-[#131842]">
+                <div className="self-center ">
+                    <p className="text-white text-3xl">Shopping App</p>
+                </div>
+                <div className="flex self-center">
+                    <a href="#" className="px-4 text-base font-medium leading-6 text-white tracking-[0.2px]">Home</a>
+                    <a href="#" className="px-4 text-base font-medium leading-6 text-white tracking-[0.2px]">Products</a>
+                    <a href="#" className="px-4 text-base font-medium leading-6 text-white tracking-[0.2px]">Your Cart</a>
+                    <a href="#" className="px-4 text-base font-medium leading-6 text-white tracking-[0.2px]">login</a>
+                </div>    
+            </div>
+        </nav>
+    );
+}
+export default Navbar;
